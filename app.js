e44babb9dfe72dc905051a1dbda5c6e344d384fe
jasandine <jasandine@gmail.com>
2015-05-23 18:17:01 -0700

    Parse data

--- app.js
+++ app.js
@@ -8,16 +8,29 @@ var options = {
   method: "GET"
 };
 
+// var login = "jasandine";
+
+function printMessage(login, public_repos) {
+  var message = login + " has " + public_repos + " public repositories";
+  console.log(message);
+}
+
 
 var req = https.request(options, function(res) {
-  console.log("statusCode: ", res.statusCode);
+  var body = "";
 
-  res.on('data', function(d) {
-    process.stdout.write(d);
+  res.on('data', function(chunk) {
+    body += chunk;
+  });
+  res.on('end', function (){
+    var profile = JSON.parse(body);
+    printMessage(profile.login, profile.public_repos)
+    console.dir(profile);
   });
 });
+
 req.end();
 
-req.on('error', function(e) {
-  console.error(e);
+req.on('error', function(error) {
+  console.error(error);
 });
