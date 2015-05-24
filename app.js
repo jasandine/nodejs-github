147b0ba30c2d6fc3da4406ed4056d5e14e3a3d5d
jasandine <jasandine@gmail.com>
2015-05-23 17:47:20 -0700

    Scrape data from my github

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello, world!");
+var https = require("https");
+
+var options = {
+  hostname: "api.github.com",
+  port: 443,
+  path: "/users/jasandine",
+  headers: {'user-agent': 'node.js'},
+  method: "GET"
+};
+
+
+var req = https.request(options, function(res) {
+  console.log("statusCode: ", res.statusCode);
+
+  res.on('data', function(d) {
+    process.stdout.write(d);
+  });
+});
+req.end();
+
+req.on('error', function(e) {
+  console.error(e);
+});
